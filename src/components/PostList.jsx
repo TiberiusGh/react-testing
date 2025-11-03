99676ce193ccdba8bf3d16c05fdbb3bca4583ff4
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-03 12:00:03 +0100

    Save current progress using state and props

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,44 @@
 import { Post } from './Post'
 import { NewPost } from './NewPost'
+import { Modal } from './Modal'
+import { useState } from 'react'
 import classes from './PostList.module.css'
 
 export function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true)
+  const [currentBody, setCurrentBody] = useState('')
+  const [enteredAuthor, setEnteredAuthor] = useState('')
+
+  function hideModalHandler() {
+    setModalIsVisible(false)
+  }
+
+  function bodyChangeHandler(event) {
+    setCurrentBody(event.target.value)
+  }
+
+  function authorChangeHandlier(event) {
+    setEnteredAuthor(event.target.value)
+  }
+
+  let modalContent
+
+  if (modalIsVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandlier}
+        />
+      </Modal>
+    )
+  }
+
   return (
     <>
-      <NewPost />
+      {modalContent}
       <ul className={classes.posts}>
-        <Post author="Tiberius" body="Hello world!" />
+        <Post author={enteredAuthor} body={currentBody} />
         <Post author="Lisa" body="Hello!" />
       </ul>
     </>
